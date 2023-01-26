0764ede93f44839bdf7f23df97e2980f3bc25e9b
Shahmirze <sahmirze.memmedyarov@gmail.com>
2023-01-26 12:23:11 +0400

    jquery css html events

--- firts/app.js
+++ firts/app.js
@@ -1,21 +1,49 @@
 const list=document.querySelector('.list')
+const center=document.querySelector('.center')
 const btn=document.getElementById('btn')
-
+const write=document.getElementById('write')
+const text=document.getElementById('text')
+const img=document.getElementById('img')
 $(document).ready(function(){
 
 	$("#btn").click(function(obj){
 		// $('.list').hide()
 		list.classList.toggle('color')
 		if($(this).text()=="click me"){
-			obj.target.innerHTML="mene bas"	
+			//obj.target.innerHTML="mene bas"	
+			$(this).html('mene bas')
+			$(this).css("color","lime")
 		}else{
-			obj.target.innerHTML="click me"
+			//obj.target.innerHTML="click me"
+			$(this).html("click me")
+			$(this).css("color","inherit")
 		}
+//set data
+$(img).attr('alt',"severde problem var")	
+console.log($(img).attr('src'),  $(img).attr('alt'))
+// get data
+console.log($(center).data('author-name'),$(center).data('year'))
+//ajax load
+// $(img).load('index.html img',function(){
+// 	console.log('sekil point Out')
+// })
+// jquery html secilmis olan obyektin textini deyiisir .html()
+// $(this) hemin elementi secmek ucun istifade olunur
 
-		
-console.log($(this).text())
 
 
+
+	})
+
+	$(write).click(function(event){
+
+		$.getJSON('result.json',function(jd){
+			console.log(jd);
+		 	$(text).html('<p>Name:'	+ jd.name+ '</p>')
+		 	$(text).append('<p>Age:'	+ jd.age+ '</p>')
+		 	$(text).append('<p>Sex:'	+ jd.sex+ '</p>')
+		})
+	
 	})
 
 	// $("#btn").dblclick(function(){
@@ -36,6 +64,7 @@ console.log($(this).text())
 	// 	console.log("mouse up	")
 	// })
 })
-// $(window).on('load',()=>{
-// 	console.log('aa mende varam')
-// })
+
+
+
+
